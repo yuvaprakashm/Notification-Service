cf10e112df0a55c012ce9def619c560d5cbfaa6c
yuvaprakash <ymahalingam@texala.net>
2026-05-12 17:05:57 +0530

    Added custom exception.

---
+++ src/main/java/net/notification/email/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package net.notification.email.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+}

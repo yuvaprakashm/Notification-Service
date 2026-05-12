86d3f2c730a59d2a64c4cbe0cafac34e149eb88b
yuvaprakash <yuvaprakashmahalingam2001@gmail.com>
2026-05-12 16:58:27 +0530

    Removed Returning Entity and added Dto.

--- src/main/java/net/notification/email/controller/NotificationController.java
+++ src/main/java/net/notification/email/controller/NotificationController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import net.notification.email.entity.Notification;
 import net.notification.email.service.NotificationService;
+import net.notification.email.vo.NotificationVo;
 
 @RestController
 @RequestMapping("/api/notification")
@@ -19,13 +20,14 @@ public class NotificationController {
 	
 	private final NotificationService notificationService;
 	
-	@GetMapping("/{userId}")
-	public List<Notification> getNotifications(@PathVariable Long userId){
-		return notificationService.getNotifications(userId);
+	@GetMapping("/user/{userId}")
+	public List<NotificationVo> getNotifications(@PathVariable Long userId,
+			@RequestParam(required = false) Boolean read) {
+		return notificationService.getNotifications(userId, read);
 	}
 	
 	@PatchMapping("{notificationId}/read")
-	public Notification markAsRead(@PathVariable Long notificationId) {
+	public NotificationVo markAsRead(@PathVariable Long notificationId) {
 		return notificationService.markAsRead(notificationId);
 	}
 }
